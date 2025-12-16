966d0334c44eddc11031395bacf6e8f839cc422f
Jafar610 <jafarmohamed955@gmail.com>
2025-12-16 13:19:17 +0300

    file updated

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const fs = require('fs');
 const http = require('http');
+ var mimeType = require('mime-types').lookup;
 
 const server = http.createServer((req,res)=>{
-    console.log("Request received");
-    res.end("Hello there");
-});
-server.listen(5002, (err)=>{
-    if(err){
-        console.log("There is an error");
+    let reqPath = req.url;
+    console.log('requested path >>>',req.url);
+    if(reqPath === '/'){
+        reqPath = '/index.html';
     }
-    console.log("Server listening");
+    let pathFile = './barber_shop' + reqPath;
+    console.log('requested file >>>', pathFile);
+    fs.readFile(pathFile,(err,content)=>{
+        if(err){
+            res.write('<h1>Page Not Found</h1>');
+            res.end();
+        }
+        else{
+            res.writeHead(200, {"content-type":mimeType(reqPath)});
+            res.write(content);
+            res.end();
+        }
+    })
+});
+
+server.listen(5001,(err)=>{
+    if(err) throw err;
+    console.log('server is running');
 })
