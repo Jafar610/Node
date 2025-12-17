45276a8b8c883f6dd8e0f63777ef52182aa36b60
Jafar610 <jafarmohamed955@gmail.com>
2025-12-17 20:38:27 +0300

    file updated

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-const fs = require('fs');
-const http = require('http');
- var mimeType = require('mime-types').lookup;
+// const fs = require('fs');
+// const http = require('http');
+// var mimeType = require('mime-types').lookup;
 
-const server = http.createServer((req,res)=>{
-    let reqPath = req.url;
-    console.log('requested path >>>',req.url);
-    if(reqPath === '/'){
-        reqPath = '/index.html';
-    }
-    let pathFile = '../barber_shop' + reqPath;
-    console.log('requested file >>>', pathFile);
-    fs.readFile(pathFile,(err,content)=>{
-        if(err){
-            res.write('<h1>Page Not Found</h1>');
-            res.end();
-        }
-        else{
-            res.writeHead(200, {"content-type":mimeType(reqPath)});
-            res.write(content);
-            res.end();
-        }
-    })
-});
+// const server = http.createServer((req, res)=>{
+//      let path = req.url;
+//      console.log("Requested path is >>> ", path);
+//      if(path === '/'){
+//         path = '/index.html';
+//      }
 
-server.listen(5001,(err)=>{
-    if(err) throw err;
-    console.log('server is running');
+//      let reqFile = '../Apple_Clone' + path;
+//      console.log("Requested File >>> ", reqFile);
+
+//      fs.readFile(reqFile,(err,content)=>{
+//         if(err){
+//             res.write('<h1> Page Not Found</h1>');
+//             res.end();
+//         }else{
+//             res.writeHead(200,{"content-type":mimeType(path)});
+//             res.write(content);
+//             res.end();
+//         }
+
+//      })
+
+// });
+
+// server.listen(5002,(err)=>{
+//     if(err){
+//         console.error(err);
+//     }
+//     console.log("Server is running");
+// })
+
+const express = require('express');
+const server = express();
+server.use(express.static("../Apple_Clone"));
+server.listen(3000,(err)=>{
+    if(err)console.error(err);
+    console.log("Server is running");
 })
